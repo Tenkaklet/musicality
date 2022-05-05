48ab64b8faf1050839af22ad938909e29c30c94d
Max Carlquist <tenkaklet@gmail.com>
2022-05-05 17:51:08 +0200

    Begun work on Explore Page

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getExplore } from "../api";
+
 
 const Explore = () => {
+    const [genre, setGenre] = useState([]);
+    const getTheGenre = async (g) => {
+        getExplore(g)
+            .then(res => {
+                console.log(g.toUpperCase());
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    };
+    useEffect(() => {
+        getTheGenre('rock');
+        getTheGenre('reggae');
+        getTheGenre('pop');
+        getTheGenre('soul');
+    }, []);
+    const listOfGenres = ['rock', 'reggae', 'pop', 'soul'];
+    const listGenres = listOfGenres.map((genre, i) => {
+            return <li key={i}><p>{genre}</p></li>;
+        });
     return (
-        <header>
-            <h1>Explore</h1>
-        </header>
+        <>
+            <header>
+                <h1>Explore</h1>
+            </header>
+            <div>
+                <ul className="genres-container">
+                    {listGenres}
+                </ul>
+            </div>
+        </>
     );
 };
 
